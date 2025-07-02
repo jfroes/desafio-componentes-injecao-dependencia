f3c899d8690c08ac6e490896f7c39c208b4a030c
José Froes <josepaulo.froes@gmail.com>
2025-07-01 21:56:15 -0300

    first commit

---
+++ src/test/java/com/josefroes/desafio_componentes_injecao_dependencia/DesafioComponentesInjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefroes.desafio_componentes_injecao_dependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioComponentesInjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
